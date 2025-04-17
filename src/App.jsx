11d1e83396065f6ce2a2a7bda7aab0a6326501fd
Mohimkhan <mohimkhan652@gmail.com>
2025-04-17 19:56:02 +0600

    feat: restructure App component to include navigation links and set up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import AutoComp from "./AutoComplete";
+import { Link } from "react-router";
 
 const App = () => {
   return (
     <>
-      <AutoComp />
+      <div className="grid place-items-center mt-8 gap-6">
+        <h2 className="text-3xl">
+          Hi, there🖐️. Here you will find all the links 🔗
+        </h2>
+
+        <ul className="flex list-decimal text-xl">
+          <li>
+            {" "}
+            <Link
+              to={"/autocomplete"}
+              prefetch="intent"
+              className="hover:underline underline-offset-4"
+            >
+              AutoComplete - component
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,20 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import AutoComp from "./AutoComplete";
 
-createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/autocomplete",
+    element: <AutoComp />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
