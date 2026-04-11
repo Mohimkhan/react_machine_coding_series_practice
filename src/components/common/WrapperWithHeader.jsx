1b5fac62a515997dbdcd6da04fb82ce2b712c013
Mohimkhan <mohimkhan652@gmail.com>
2026-04-11 20:07:14 +0600

    style(WrapperWithHeader): the wrapper should center all the childrens and take full width

--- src/components/common/WrapperWithHeader.jsx
+++ src/components/common/WrapperWithHeader.jsx
@@ -1,8 +1,9 @@
 const WrapperWithHeader = ({ heading = "Something Something", children }) => {
   return (
-    <div className="grid place-content-center">
-      <h2 className="text-xl font-bold mt-10 mb-5">{heading}</h2>
-      {children}
+    <div className="w-full flex flex-col items-center">
+      <h2 className="text-xl font-bold mt-10 mb-5 text-center">{heading}</h2>
+
+      <div className="w-full max-w-4xl">{children}</div>
     </div>
   );
 };
